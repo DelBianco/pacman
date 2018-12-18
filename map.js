a20242f70b88e199022e1ccb129a3e61c1fd5626
André Giuffrida <andre.giuffrida@runweb.com.br>
2018-12-18 13:56:20 -0200

    Start

---
+++ map.js
@@ -0,0 +1,7 @@
+class Map {
+    constructor(altura, largura) {
+        this.h = altura; this.w = largura;
+        createCanvas(this.h,this.w);
+        background(33);
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,9 @@
+var map;
+
+function setup() {
+    map = new Map(300, 400);
+}
+
+function draw() {
+    //background(220);
+}
